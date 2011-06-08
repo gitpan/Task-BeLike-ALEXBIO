b2395a4d90d3e4deef06dda99068f8625584d6bf
Alessandro Ghedini <alexbio@cpan.org>
2011-06-08 14:46:52 +0000

    Import of ALEXBIO/Task-BeLike-ALEXBIO-0.04 from CPAN.
    gitpan-cpan-distribution: Task-BeLike-ALEXBIO
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         ALEXBIO/Task-BeLike-ALEXBIO-0.04.tar.gz
    gitpan-cpan-author:       ALEXBIO
    gitpan-cpan-maturity:     released

--- lib/Task/BeLike/ALEXBIO.pm
+++ lib/Task/BeLike/ALEXBIO.pm
@@ -1,192 +1,42 @@
 package Task::BeLike::ALEXBIO;
+BEGIN {
+  $Task::BeLike::ALEXBIO::VERSION = '0.04';
+}
 
 use warnings;
 use strict;
 
 =head1 NAME
 
-Task::BeLike::ALEXBIO - Useful modules list
+Task::BeLike::ALEXBIO - A bunch of modules I use
 
 =head1 VERSION
 
-Version 0.03
-
-=cut
-
-our $VERSION = '0.03';
-
+version 0.04
 
 =head1 SYNOPSIS
 
-This module installs many useful dependencies
+    cpanm Task::BeLike::ALEXBIO
 
-=head1 TASK CONTENTS
+=head1 DESCRIPTION
 
-=head2 AnyEvent Framework
+Install modules that I want on a fresh perl installation.
 
-=over 4
+    L<App::gist>
+    L<Dist::Zilla::PluginBundle::ALEXBIO>
+    L<Module::CheckDeps>
+    L<Text::Todo::Simple>
 
-=item * L<AnyEvent>
-
-=item * L<AnyEvent::HTTPD>
-
-=back
-
-=head2 Apps
-
-=over 4
-
-=item * L<App::Ack>
-
-=item * L<App::TwitShell>
-
-=item * L<Text::Todo::Simple>
-
-=back
-
-=head2 Compression
-
-=over 4
-
-=item * L<Compress::LZMA::External>
-
-=back
-
-=head2 CPAN
-
-=over 4
-
-=item * L<CPAN::Mini>
-
-=item * L<CPAN::SQLite>
-
-=item * L<YAML>
-
-=back
-
-=head2 Encryption
-
-=over 4
-
-=item * L<Crypt::Blowfish>
-
-=item * L<Crypt::CBC>
-
-=item * L<Crypt::RandPasswd>
-
-=back
-
-=head2 Development
-
-=over 4
-
-=item * L<Devel::NYTProf>
-
-=item * L<Devel::Trace>
-
-=item * L<Moose>
-
-=item * L<Perl::Critic>
-
-=item * L<Regexp::Common>
-
-=back
-
-=head2 JSON
-
-=over 4
-
-=item * L<JSON>
-
-=item * L<JSON:PP>
-
-=item * L<JSON::XS>
-
-=back
-
-=head2 Module
-
-=over 4
-
-=item * L<Module::CheckDeps>
-
-=item * L<Module::Starter>
-
-=item * L<Module::Starter::Smart>
-
-=back
-
-=head2 Testing
-
-=over 4
-
-=item * L<Test::Pod>
-
-=item * L<Test::Pod::Coverage>
-
-=item * L<Test::More>
-
-=back
-
-=head2 Web
-
-=over 4
-
-=item * L<LWP>
-
-=item * L<WWW::GitHub::Gist>
-
-=back
-
-=head2 XML
-
-=over 4
-
-=item * L<XML::Simple>
-
-=back
+Most of the "heavier" modules (like L<Moose> and L<Dist::Zilla>), are installed
+from the L<Debian|http://www.debian.org> archive.
 
 =head1 AUTHOR
 
-Alessandro Ghedini, C<< <alexbio at cpan.org> >>
-
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-task-belike-alexbio at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Task-BeLike-ALEXBIO>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc Task::BeLike::ALEXBIO
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Task-BeLike-ALEXBIO>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/Task-BeLike-ALEXBIO>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/Task-BeLike-ALEXBIO>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/Task-BeLike-ALEXBIO/>
-
-=back
+Alessandro Ghedini <alexbio@cpan.org>
 
 =head1 LICENSE AND COPYRIGHT
 
-Copyright 2010 Alessandro Ghedini.
+Copyright 2011 Alessandro Ghedini.
 
 This program is free software; you can redistribute it and/or modify it
 under the terms of either: the GNU General Public License as published
