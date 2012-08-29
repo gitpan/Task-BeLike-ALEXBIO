87a4286c3c796c9629e11492768f5be0ccfd15ff
Alessandro Ghedini <alexbio@cpan.org>
2012-08-29 19:43:12 +0000

    Import of ALEXBIO/Task-BeLike-ALEXBIO-0.09 from CPAN.
    gitpan-cpan-distribution: Task-BeLike-ALEXBIO
    gitpan-cpan-version:      0.09
    gitpan-cpan-path:         ALEXBIO/Task-BeLike-ALEXBIO-0.09.tar.gz
    gitpan-cpan-author:       ALEXBIO
    gitpan-cpan-maturity:     released

--- lib/Task/BeLike/ALEXBIO.pm
+++ lib/Task/BeLike/ALEXBIO.pm
@@ -1,6 +1,6 @@
 package Task::BeLike::ALEXBIO;
 {
-  $Task::BeLike::ALEXBIO::VERSION = '0.08';
+  $Task::BeLike::ALEXBIO::VERSION = '0.09';
 }
 
 use strict;
@@ -12,7 +12,7 @@ Task::BeLike::ALEXBIO - Stuff I use
 
 =head1 VERSION
 
-version 0.08
+version 0.09
 
 =head1 DESCRIPTION
 
